74065abe5ae83fbcc4a8faeddc28e6adc6c8971e
itexperts-ai <itexperts.ai@gmail.com>
2025-07-15 15:49:11 +0530

    Delete UserAuthentication directory

--- UserAuthentication/src/main/java/UserAuthentication/Exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package UserAuthentication.Exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
-
