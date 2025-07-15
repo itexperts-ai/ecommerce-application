8dee71bb7d53950d615c7fb8cfa65f0b5aa5bbb4
itexperts-ai <itexperts.ai@gmail.com>
2025-07-15 14:07:12 +0530

    order service history changes

--- orderservice/src/main/java/com/example/orderservice/Service/OrderHistoryService.java
+++ orderservice/src/main/java/com/example/orderservice/Service/OrderHistoryService.java
@@ -1,8 +1,10 @@
 package com.example.orderservice.Service;
 
+import org.springframework.stereotype.Service;
+
 import java.util.List;
 import java.util.Map;
-
+@Service
 public interface OrderHistoryService {
     List<Map<String, Object>>getAllOrdersHistoryStatus();
 }
